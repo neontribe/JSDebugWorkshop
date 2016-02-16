97bf241efec9f50d8c25ae2c777c552db6790c1c
Robert Preus-MacLaren <rpprroger@googlemail.com>
2016-02-16 17:43:54 +0000

    Attach backend and database API

--- api.js
+++ api.js
@@ -1,8 +1,51 @@
 var Store = require('./store');
+var DEV = process.env.NODE_ENV !== 'production';
 
 function extend(a, b) {
-	var ab = {};
-	Object.keys(a).concat(Object.keys(b)).forEach(function() {});
+	if (arguments.length <= 2) {
+		var ab = {};
+		var ay = a || {};
+		var bee = b || {};
+		Object.keys(ay).concat(Object.keys(bee)).forEach(function(key) {
+			ab[key] = bee[key] || ay[key];
+		});
+		return ab;
+	} else {
+		// ಠ_ಠ
+		return extend(a, extend.apply(null, [].slice.call(arguments, 1)));
+	}
+}
+
+function extractBody(req, cb) {
+	var b = '';
+	req.on('data', function(buffer) {
+		b += buffer.toString(); // implicit/magic with +=, but this is more readable...
+	});
+
+	req.on('end', function() {
+		cb(null, b);
+	});
+
+	req.on('error', function(err) {
+		cb(err);
+	});
+}
+
+function processBody(req, cb) {
+	var ct = req.headers['content-type'];
+
+	function done(err, body) {
+		cb(err, body);
+	}
+	switch(ct) {
+		case 'application/json':
+			extractBody(req, function(err, body) {
+				done(err, JSON.parse(body));
+			});
+		break;
+		default:
+			cb(new Error('Bad content type, supports: `' + ['application/json'].join('`, `') + '`'));
+	}
 }
 
 function API(dbPath) {
@@ -11,24 +54,112 @@ function API(dbPath) {
 	this.hook = this.hook.bind(this);
 }
 
-API.prototype.respondWithJSON = function respondWithJSON(headerOverrides, data, res) {
-	var overrides = headerOverrides || {};
-	var headers = {
-		status: 200,
+API.prototype.respondWithJSON = function respondWithJSON(res, data, statusOverride, headerOverrides) {
+	var json = JSON.stringify(data, null, DEV ? '\t' : '');
+
+	this.respond(res, json, statusOverride, extend({
 		'Content-Type': 'application/json'
-	};
+	}, headerOverrides));
+};
+
+API.prototype.respondWithError = function respondWithError(res, err, status) {
+	var msg = err instanceof Error ? err.message : err;
+	this.respondWithJSON(res, {message: msg}, status || 400);
+};
+
+API.prototype.respond = function respond(res, str, statusOverride, headerOverrides) {
+	if (!res) {
+		throw new Error('Missing response');
+	}
+	var headers = extend({
+		'Content-Length': Buffer.byteLength(str || '')
+	}, headerOverrides);
+	var status = statusOverride || 200;
+
+	res.writeHead(status, 'success', headers);
+	res.end(str);
 };
 
 API.prototype.hook = function hook(op, req, res) {
 	var cmds = op.split('/');
-	var type = cmds[0];
+	var id = cmds[0];
+	var type = req.method.toLowerCase();
+	var respondWithJSON = this.respondWithJSON.bind(this);
+	var respondWithError = this.respondWithError.bind(this);
+	var todos = this.todos;
 
 	switch(type) {
 		case 'get':
-		break;
-		case 'create':
+			if (id) {
+				todos.findOne(id, function(err, data) {
+					if (err) {
+						console.error(err);
+						return respondWithError(res, err, 404);
+					}
+					respondWithJSON(res, data);
+				});
+			} else {
+				todos.findAll(function(err, data) {
+					if (err) {
+						console.error(err);
+						return respondWithError(res, err);
+					}
+					respondWithJSON(res, data);
+				});
+			}
 		break;
 		case 'put':
+			processBody(req, function(err, body) {
+				if (err) {
+					console.error(err);
+					return respondWithError(res, err);
+				}
+				todos.insert(body, function(err, record) {
+					if (err) {
+						console.log(err);
+						return respondWithError(res, err);
+					}
+					respondWithJSON(res, record);
+				});
+			});
+		break;
+		case 'patch':
+			if (id) {
+				respondWithError(res, 'missing id data from url fragment');
+			} else {
+				todos.findOne(id, function(err, record) {
+					if (err) {
+						console.error(err);
+						return respondWithError(res, err, 404);
+					}
+					processBody(req, function(err, body) {
+						if (err) {
+							console.error(err);
+							return respondWithError(res, err);
+						}
+						todos.update(id, extend(record, body), function(err, record) {
+							if (err) {
+								console.error(err);
+								return respondWithError(res, err);
+							}
+							respondWithJSON(res, record);
+						});
+					});
+				});
+			}
+		break;
+		case 'delete':
+			if (id) {
+				respondWithError(res, 'missing id data from url fragment');
+			} else {
+				todos.remove(id, function(err, record) {
+					if (err) {
+						console.error(err);
+						return respondWithError(res, err);
+					}
+					respondWithJSON(res, record);
+				});
+			}
 		break;
 	}
 };
--- store.js
+++ store.js
@@ -1,20 +1,50 @@
 var fs = require('fs');
-var strSep = process.env.NODE_ENV === 'production' ? '' : '\t';
+var DEV = process.env.NODE_ENV !== 'production';
 
-function noop() {}
-function cbSafe(fn) {return fn || noop;}
+function throwOrNoop(err) {
+	if (err) {
+		if (!(err instanceof Error)) {
+			err = new Error('Something exploded, this is all we have: ' + err);
+		}
+		throw err;
+	}
+}
+function cbSafe(fn) {return fn || throwOrNoop;}
+
+function saveStore(path, data, cb) {return fs.writeFile(path, JSON.stringify(data, null, DEV ? '\t' : ''), cb);}
+function saveStoreSync(path, data) {return fs.writeFileSync(path, JSON.stringify(data, null, DEV ? '\t' : ''));}
+function clone(d) {return JSON.parse(JSON.stringify(d));}
 
 function loadOrCreateStore(path, callback) {
 	var cb = cbSafe(callback);
 
-	fs.readFile(this._path, function(err, data) {
-		if (err) throw err;
-		console.log(data);
-	});
-}
+	function don(err, data) {
+		if (err) {
+			return cb(err);
+		}
+		var dat = data.toString();
+		try {
+			dat = JSON.parse(dat);
+		} catch(e) {
+			console.log(e);
+			throw new Error('Could not parse data at provided path (' + path + '), quiting...');
+		}
+		console.log('imported store');
 
-function saveSync(path, data) {
-	return fs.writeFileSync(path, JSON.stringify(data, null, strSep));
+		return cb(err, dat);
+	}
+
+	fs.readFile(path, function(err, data) {
+		if (err) {
+			if (err.code === 'ENOENT') {
+				console.log('no store, creating one at (' + path + ')');
+				return saveStore(path, {}, function(err) {
+					don(err, '{}'); // ಠ_ಠ
+				});
+			}
+		}
+		return don(err, data);
+	});
 }
 
 function Store(path, name, callback) {
@@ -22,29 +52,56 @@ function Store(path, name, callback) {
 	this._path = path;
 	this._name = name;
 
-	loadOrCreateStore(path, (function(store) {
+	var saveSync = this.saveSync.bind(this);
+	this._cleanup = [];
+
+	function sigint() {
+		saveSync();
+		// let other bindings do _synchronous_ things
+		setImmediate(function() {
+			process.exit();
+		});
+	}
+	function uncaughtException(err) {
+		saveSync();
+		throw err instanceof Error ? err : new Error(err);
+	}
+
+	this._bindProcessAndAddCleanup('exit', saveSync);
+	this._bindProcessAndAddCleanup('SIGINT', sigint);
+	this._bindProcessAndAddCleanup('uncaughtException', uncaughtException);
+
+	loadOrCreateStore(path, (function(err, store) {
 		this._store = store;
-		cb(store);
+		cb(err, store);
 	}).bind(this));
-
-	var cleanup = this.cleanup = this.cleanup.bind(this);
-
-	this.exitEvents.forEach(function(evt) {
-		process.on(evt, cleanup);
-	});
 }
 
-Store.prototype.exitEvents = ['exit', 'SIGINT', 'uncaughtException'];
-
-Store.prototype.cleanup = function cleanup() {
-	saveSync(this._path, this._store);
-	this.exitEvents.forEach(function(evt) {
-		// this.cleanup should be an exact reference
-		process.removeListener(evt, this.cleanup);
+Store.prototype._bindProcessAndAddCleanup = function _bindProcessAndAddCleanup(evt, cb) {
+	process.on(evt, cb);
+	this._cleanup.push(function() {
+		process.removeListener(evt, cb);
 	});
 };
 
-Store.prototype.getTable = function getTable() {
+Store.prototype.close = function close() {
+	this.saveSync();
+	this._cleanup.forEach(function(fn) {fn();});
+};
+
+Store.prototype.saveSync = function saveSync() {
+	if (this._store) {
+		saveStoreSync(this._path, this._store);
+	}
+};
+
+Store.prototype.save = function save(cb) {
+	if (this._store) {
+		saveStore(this._path, this._store, cb);
+	}
+};
+
+Store.prototype._getTable = function _getTable() {
 	var table = this._store[this._name];
 	if (!table) {
 		this._store[this._name] = table = [];
@@ -57,19 +114,73 @@ Store.prototype.find = function find(query, callback) {
 	var cb = cbSafe(callback);
 
 	if (keys && keys.length) {
-		var table = this.getTable();
-		cb.call(this, table.filter(function(row) {
+		var table = this._getTable();
+		cb.call(this, null, clone(table.filter(function(row) {
 			return Object.keys(query).every(function(key) {
 				return row[key] === query[row];
 			});
-		}));
+		})));
 	} else {
-		cb.call(this, []);
+		cb.call(this, null, []);
 	}
 };
 
+Store.prototype.findOne = function findOne(id, callback) {
+	var cb = cbSafe(callback);
+	this.find({id: id}, function(err, found) {
+		if (err) {
+			return cb.call(this, err);
+		}
+		if (found[0]) {
+			cb.call(this, null, found[0]);
+		} else {
+			cb.call(this, new Error(''));
+		}
+	});
+};
+
 Store.prototype.findAll = function findAll(callback) {
-	return cbSafe(callback).call(this, this.getTable());
+	return cbSafe(callback).call(this, null, clone(this._getTable()));
+};
+
+Store.prototype._insert = function _insert(record, callback) {
+	var cb = cbSafe(callback);
+
+	this._getTable().push(record);
+	cb.call(this, null, record);
+};
+
+Store.prototype.insert = function insert(record, callback) {
+	var cb = cbSafe(callback);
+	var cloned = clone(record);
+	cloned.id = Date.now();
+
+	this._insert(record, cb);
+};
+
+Store.prototype.remove = function remove(id, callback) {
+	var cb = cbSafe(callback);
+	this.findOne(id, function(err, found) {
+		if (err) {
+			return cb.call(this, err);
+		}
+		var table = this._getTable();
+		var i = table.indexOf(found);
+		table[i] = null;
+		cb.call(this, null, found);
+	});
+};
+
+Store.prototype.update = function update(id, newData, callback) {
+	var cb = cbSafe(callback);
+	this.remove(id, function(err) {
+		if (err) {
+			return cb.call(this, err);
+		}
+		var cloned = clone(newData);
+		cloned.id = id;
+		this._insert(cloned);
+	});
 };
 
 module.exports = Store;
